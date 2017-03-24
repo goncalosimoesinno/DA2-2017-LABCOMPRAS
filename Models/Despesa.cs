18bba4a460bd22b8535dce8cea1a45a1ff1473d1
150323044 <150323044@ESCE-D216-08>
2017-03-24 17:32:51 +0000

    Add project files.

---
+++ Models/Despesa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DA2_2017_LABCOMPRAS.Models
+{
+    public class Despesa
+    {
+        private string NomeDespesa;
+        private int Quantidade;
+        private double valor;
+        private string Categoria;
+        private bool Pago;
+    }
+}
